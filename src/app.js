87996e4c9e36dd7a28e0e49fcc0c6db110e0d4a0
AgneskaD <84664209+AgneskaD@users.noreply.github.com>
2021-07-27 12:50:29 +0200

    added API

--- src/app.js
+++ src/app.js
@@ -49,7 +49,14 @@ function formatDate(timestamp) {
   
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-    console.log(forecastHTML);
+   
+  }
+  
+  function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "eb5c72c24c9c8365c67bb1e6c96a1978";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   }
 
   function displayTemperature(response) {
@@ -74,8 +81,10 @@ function formatDate(timestamp) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
-  }
-  
+ 
+  getForecast(response.data.coord);
+   }
+
   function search(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
