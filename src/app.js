01c6dfcc2a28c15c3033d111387853ceec37ba6e
AgneskaD <84664209+AgneskaD@users.noreply.github.com>
2021-07-27 12:24:13 +0200

    edited forcast

--- src/app.js
+++ src/app.js
@@ -23,29 +23,35 @@ function formatDate(timestamp) {
   }
 
   function displayForecast() {
-      let forecastElement = document.querySelector("#forecast");
-
-      forecastElement.innerHTML = `
-      <div class="row">
-         <div class="col-2">
-         <div class="weather-forecast-date">
-             Thursday
-             </div>
-             <img src="http://openweathermap.org/img/wn/04d@2x.png"
-             alt=""
-             width="42"
-             />
-             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">
-             18° </span>
-             <span class="weather-forecast-temperature-min">
-             12° </span>
-             </div>
-           </div>
-          </div>
-      `;
-  }
+    let forecastElement = document.querySelector("#forecast");
   
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+  }
+
   function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -112,3 +118,4 @@ function formatDate(timestamp) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   search("Koszalin");
+  displayForecast();
