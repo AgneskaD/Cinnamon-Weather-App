eba21fc5b91d09e546e9f298d43c7bf9451b3b1a
AgneskaD <84664209+AgneskaD@users.noreply.github.com>
2021-07-27 11:01:43 +0200

    moved forecast to js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,30 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
+
+  function displayForecast() {
+      let forecastElement = document.querySelector("#forecast");
+
+      forecastElement.innerHTML = `
+      <div class="row">
+         <div class="col-2">
+         <div class="weather-forecast-date">
+             Thursday
+             </div>
+             <img src="http://openweathermap.org/img/wn/04d@2x.png"
+             alt=""
+             width="42"
+             />
+             <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">
+             18° </span>
+             <span class="weather-forecast-temperature-min">
+             12° </span>
+             </div>
+           </div>
+          </div>
+      `;
+  }
   
   function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
